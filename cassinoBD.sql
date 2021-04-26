1a57cf352499de4db6855607110a8e450f329837
Well <wellwlds@hotmail.com>
2021-04-26 00:55:00 -0300

    fix: fixing database and inserts. Now its running

--- cassinoBD.sql
+++ cassinoBD.sql
@@ -1,4 +1,8 @@
-CREATE TABLE j2ee.user(
+drop schema wellson cascade;
+
+create schema wellson;
+
+CREATE TABLE wellson.user(
     id serial NOT NULL,
     login character varying(20) NOT NULL,
     senha character(32) NOT NULL,
@@ -12,20 +16,20 @@ CREATE TABLE j2ee.user(
     CONSTRAINT uq_user_login UNIQUE (login)
 );
 
-CREATE TABLE j2ee.games(
+CREATE TABLE wellson.games(
     id serial NOT NULL,
     nome character(32) NOT NULL,
     multiplicador integer DEFAULT 1,
-    CONSTRAINT pk_user PRIMARY KEY (id)
+    CONSTRAINT pk_user_games PRIMARY KEY (id)
 );  
 
-CREATE TABLE j2ee.log(
+CREATE TABLE wellson.log(
     id serial NOT NULL,
     user_id serial NOT NULL,
     game_id serial NOT NULL,
     house_gain boolean,
     money integer DEFAULT 0,
-    CONSTRAINT pk_user PRIMARY KEY (id)
+    CONSTRAINT pk_user_log PRIMARY KEY (id)
 );
 
 CREATE OR REPLACE FUNCTION totalGanhos(_user_id integer,_game_id integer)
@@ -49,31 +53,31 @@ END;
 $total$ LANGUAGE plpgsql;
 
 
-INSERT INTO j2ee.user VALUES
+INSERT INTO wellson.user VALUES
 	(1, 'teste', MD5('1234'), 'Marcos Santos', '04-07-1999', NULL, 125, '04-07-1999', '07-09-1999'),
 	(2, 'well', MD5('1234'), 'Wellington Silva', '05-04-1974', NULL, 278, '05-04-1974', '14-10-1999'),
 	(7, 'andy', MD5('1234'), 'Anderson Makino', '21-01-1985', NULL, 291, '23-01-1985', '13-10-1999');
 
 
-INSERT INTO j2ee.games VALUES
+INSERT INTO wellson.games VALUES
 	(1, 'blackjack', 2),
 	(2, 'slotmachine', 5);
 	
-INSERT INTO j2ee.log VALUES
-	(1, 1, 1, 1,  15),
-	(2, 1, 1, 0,  25),
-	(3, 1, 2, 1,  5),
-	(4, 1, 1, 0,  47),
-	(5, 1, 2, 0,  5),
+INSERT INTO wellson.log VALUES
+	(1, 1, 1, true,  15),
+	(2, 1, 1, false,  25),
+	(3, 1, 2, true,  5),
+	(4, 1, 1, false,  47),
+	(5, 1, 2, false,  5),
 	
-	(6, 2, 1, 0,  13),
-	(7, 2, 1, 1,  60),
-	(8, 2, 2, 1,  5),
-	(9, 2, 2, 1,  5),
-	(10, 2, 2, 1,  5),
+	(6, 2, 1, false,  13),
+	(7, 2, 1, true,  60),
+	(8, 2, 2, true,  5),
+	(9, 2, 2, true,  5),
+	(10, 2, 2, true,  5),
 	
-	(11, 3, 2, 0,  5),
-	(12, 3, 2, 0,  5),
-	(13, 3, 2, 0,  5),
-	(14, 3, 2, 1,  5),
-	(15, 3, 2, 1,  5);
+	(11, 3, 2, false,  5),
+	(12, 3, 2, false,  5),
+	(13, 3, 2, false,  5),
+	(14, 3, 2, true,  5),
+	(15, 3, 2, true,  5);
